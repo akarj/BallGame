4ab0ee381a9c6ad183617856c2e24f8a0ad8eae2
Anuj Kumar <kumaranuj.work@gmail.com>
2024-09-06 14:04:23 +0530

    label was centerd

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,13 +18,13 @@ const Header = ({
 				<button className="w-32" onClick={resetHandler}>
 					Reset
 				</button>
-				<div className="interval-container flex justify-center text-center flex-row gap-4 font-semibold">
+				<div className="interval-container flex justify-center items-center text-center flex-row gap-4 font-semibold">
 					<label htmlFor="interval">Interval (in sec)</label>
 					<input
 						type="number"
 						value={inputVal}
 						onChange={onChangeValHandler}
-						className="pl-2 mt-3"
+						className="pl-2"
 						id="interval"
 					/>
 				</div>
